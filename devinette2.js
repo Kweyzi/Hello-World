660cd7698568d3e1c879faf308aaa15d494fc54e
Kweyzi <jazi.skander@gmail.com>
2016-09-14 11:54:06 +0200

    Add files via upload

---
+++ devinette2.js
@@ -0,0 +1,35 @@
+/* 
+Activité : jeu de devinette
+*/
+
+// NE PAS MODIFIER OU SUPPRIMER LES LIGNES CI-DESSOUS
+// COMPLETEZ LE PROGRAMME UNIQUEMENT APRES LE TODO
+
+console.log("Bienvenue dans ce jeu de devinette !");
+
+// Cette ligne génère aléatoirement un nombre entre 1 et 100
+var solution = Math.floor(Math.random() * 100) + 1;
+
+// Décommentez temporairement cette ligne pour mieux vérifier le programme
+console.log("(La solution est " + solution + ")");
+
+
+//TODO : complétez le programme
+
+var x = Number(prompt("Devine entre 0 et 100"));
+var i = 6; // Nombre d'essais possibles
+
+while ((x !== solution) || (i < 0)) {
+    var x = Number(prompt("Devine encore! Il te reste " + i + " essais!")); // Je n'ai pas réussi à me débarasser de cette redondance je pensais que rajouter x; aurait suffi mais apparemment non.
+    i = i - 1;
+    if (x < solution) {
+            console.log(x + " est trop petit!");
+    } else {
+            console.log(x + " est trop grand!");
+    }
+}
+if (x === solution) {
+    console.log("Bravo! La bonne réponse était : " + solution + "! Il te reste " + i + " essais!");
+} else {
+    console.log("Perdu! La bonne réponse était " + solution + " ! Tu as utilisé tes 6 essais!");
+}
